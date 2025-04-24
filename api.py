ca984d26b2304894bd55d30530151ff1f5fbfd8f
Toan-afk <srcnro0606@gmail.com>
2025-04-24 11:51:30 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,25 +1,13 @@
+
 from flask import Flask, jsonify
-import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route("/giavang")
 def giavang():
-    try:
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
-        res = requests.get("https://giavang.doji.vn", headers=headers, timeout=5)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        table = soup.find("table", class_="table-price")
-        rows = table.find_all("tr")
+    return jsonify({
+        "giavang": "SJC Mua: 116.200.000 | Bán: 117.500.000"
+    })
 
-        for row in rows:
-            cols = row.find_all("td")
-            if cols and "DOJI HN" in cols[0].text:
-                mua = cols[1].text.strip()
-                ban = cols[2].text.strip()
-                return jsonify({"giavang": f"DOJI HN Mua: {mua} | Bán: {ban}"})
-    except Exception as e:
-        return jsonify({"giavang": "Không lấy được giá!"})
+if __name__ == "__main__":
+    app.run()
